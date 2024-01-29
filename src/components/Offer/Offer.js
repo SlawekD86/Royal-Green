2fcf5edfeb79a6b08a0d849f7ca2114071c12db7
Slawek Derabin <slawomirderabin@gmail.com>
2024-01-29 18:35:57 +0100

    add responsive to Offer

--- src/components/Offer/Offer.js
+++ src/components/Offer/Offer.js
@@ -1,57 +1,98 @@
 import React from 'react';
+import styled from 'styled-components';
 import imageOferta from '../../assets/imageOferta.jpg';
 import imageAranzacje from '../../assets/imageAranzacje.jpg';
 import imageService from '../../assets/imageService.jpg';
 
+const Section = styled.section`
+    display: flex;
+    flex-direction: column;
+    background-color: black;
+    color: white;
+    padding-bottom: 30px;
+`;
+
+const FlexContainer = styled.div`
+    display: flex;
+
+    @media (max-width: 450px) {
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+    }
+`;
+
+const Image = styled.img`
+    width: 50%;
+    height: auto;
+
+    @media (max-width: 450px) {
+        width: 80%;
+        border-radius: 50%;
+        margin-bottom: 20px;
+    }
+`;
+
+const ContentContainer = styled.div`
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 20px;
+    text-align: justify;
+`;
+
+const TextContainer = styled.div`
+    max-width: 470px;
+    line-height: 1.6;
+`;
+
+// Nowa sekcja Aranżacje z innymi stylami
+const AranzacjeContainer = styled(FlexContainer)`
+    @media (max-width: 450px) {
+        flex-direction: column-reverse;
+        align-items: center;
+        text-align: center;
+    }
+`;
+
 const Offer = () => {
     return (
-        <section id="offer" style={{ display: 'flex', flexDirection: 'column', backgroundColor: 'black', color: 'white', paddingBottom: '30px' }}>
-            <div style={{ display: 'flex' }}>
-                <img
-                    src={imageOferta}
-                    alt="imageOferta"
-                    style={{ width: '50%', height: 'auto' }}
-                />
-                <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', textAlign: 'justify' }}>
-                    <div style={{ maxWidth: '470px', lineHeight: '1.6' }}>
+        <Section id="offer">
+            <FlexContainer>
+                <Image src={imageOferta} alt="imageOferta" />
+                <ContentContainer>
+                    <TextContainer>
                         <h2 style={{ fontSize: '35px', margin: '16px' }}>Oferta</h2>
                         <p style={{ fontSize: '20px', margin: '0' }}>
                             Oferujemy kompleksową obsługę aranżacyjną, dostosowaną do indywidualnych potrzeb klienta. Nasza praca opiera się na pasji do detali i dbałości o estetykę oraz funkcjonalność otoczenia. Staramy się stworzyć harmonijną przestrzeń, która zachwyca i spełnia oczekiwania naszych klientów.
                         </p>
-                    </div>
-                </div>
-            </div>
-            <div style={{ display: 'flex' }}>
-                <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', textAlign: 'justify' }}>
-                    <div style={{ maxWidth: '470px', lineHeight: '1.6' }}>
+                    </TextContainer>
+                </ContentContainer>
+            </FlexContainer>
+            <AranzacjeContainer>
+                <ContentContainer>
+                    <TextContainer>
                         <h2 style={{ fontSize: '35px', margin: '16px' }}>Aranżacje</h2>
                         <p style={{ fontSize: '20px', margin: '0' }}>
                             Nasze aranżacje to wynik starannego doboru roślin i ich umiejscowienia, tworząc kompozycje doskonale wpasowujące się w charakter danego miejsca. Dbamy o każdy detal, tworząc przyjemne otoczenie, które inspiruje i pozwala na pełne wykorzystanie potencjału przestrzeni.
                         </p>
-                    </div>
-                </div>
-                <img
-                    src={imageAranzacje}
-                    alt="imageAranzacje"
-                    style={{ width: '50%', height: 'auto' }}
-                />
-            </div>
-            <div style={{ display: 'flex' }}>
-                <img
-                    src={imageService}
-                    alt="imageService"
-                    style={{ width: '50%', height: 'auto' }}
-                />
-                <div style={{ flex: 1, display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', textAlign: 'justify' }}>
-                    <div style={{ maxWidth: '470px', lineHeight: '1.6' }}>
+                    </TextContainer>
+                </ContentContainer>
+                <Image src={imageAranzacje} alt="imageAranzacje" />
+            </AranzacjeContainer>
+            <FlexContainer>
+                <Image src={imageService} alt="imageService" />
+                <ContentContainer>
+                    <TextContainer>
                         <h2 style={{ fontSize: '35px', margin: '16px' }}>Service</h2>
                         <p style={{ fontSize: '20px', margin: '0' }}>
                             Nasze usługi serwisu roślin obejmują pełną opiekę, w tym regularne podlewanie, dokładne nawożenie oraz monitorowanie ich stanu zdrowia. Dodatkowo zapewniamy przesadzanie roślin, uzupełnianie braków w glebie, wymianę podłoża oraz regularne oceny ich kondycji. Dzięki naszemu doświadczeniu i trosce o rośliny zapewniamy im nie tylko estetyczny wygląd, ale także zdrowy i korzystny dla ich rozwoju środowisko.
                         </p>
-                    </div>
-                </div>
-            </div>
-        </section>
+                    </TextContainer>
+                </ContentContainer>
+            </FlexContainer>
+        </Section>
     );
 };
 
